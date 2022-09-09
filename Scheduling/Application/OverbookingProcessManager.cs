d23513bc204292e435c2eabe5f284f22667eedfc
Waylon Martinez <waylon.martinez@codesulting.com>
2022-09-09 07:46:35 -0500

    worked through process manage training tasks

--- Scheduling/Application/OverbookingProcessManager.cs
+++ Scheduling/Application/OverbookingProcessManager.cs
@@ -15,17 +15,26 @@ public class OverbookingProcessManager : EventHandler
     {
         When<SlotScheduled>(async (e, m) =>
         {
-                
+            await bookedSlotRepository.AddSlot(new BookedSlot(e.SlotId.ToString(), e.DayId, e.SlotStartTime.Month));
         });
 
         When<SlotBooked>(async (e, m) =>
         {
+            await bookedSlotRepository.MarkSlotAsBooked(e.SlotId.ToString(), e.PatientId);
 
+            var slot = await bookedSlotRepository.GetSlot(e.SlotId.ToString());
+            var count = await bookedSlotRepository.CountByPatientAndMonth(e.PatientId, slot.Month);
+
+            if (count > bookingLimitedPerPatient)
+            {
+                await commandStore.Send(new CancelSlotBooking(e.DayId, e.SlotId, "overbooked"),
+                    new CommandMetadata(m.CorrelationId, new CausationId(idGenerator())));
+            }
         });
 
         When<SlotBookingCancelled>(async (e, m) =>
         {
-                
+            await bookedSlotRepository.MarkSlotAsAvailable(e.SlotId.ToString());
         });
     }
 }
